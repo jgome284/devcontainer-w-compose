e2b0fc1f82641ed5bc2679e9d86e5a8277855289
Jose M. Gomez <gomezjosem@live.com>
2023-12-19 16:31:08 -0500

    removed blurb

--- src/main.py
+++ src/main.py
@@ -1,2 +1,2 @@
 import cowsay
-cowsay.cow('Hello World! I built a gnarly devcontainer using docker-compose for use on VS code! Alright')
+cowsay.cow('Hello World! I built a gnarly devcontainer using docker-compose for use on VS code!')
